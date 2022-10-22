2f2fca933ef5552684fb6dc1171a3f03870da559
Manel <dias_ricardo46@hotmail.com>
2022-10-22 18:39:02 +0100

    add score tracker and final result to code

--- js.js
+++ js.js
@@ -1,12 +1,16 @@
 // First the user is gonna input one of three choices. We'll do this with a prompt.
 // Then the computer is gonna output one of the same three choices.
-// We're gonna play a round.
+// We're gonna play a round. ----- This has been scratched, it's 5 rounds now.
 // if both choices are the same, it's a tie
 // It's a win (to the user) if: paper-rock, scissors-paper, rock-scissors.
+//Next we're gonna make it more complex. We're gonna play 5 rounds.
+//At the end display the winner
+
+
 
 // First the user is gonna input one of three choices. We'll do this with a prompt.
-let playerChoice = prompt("What is your choice? ", "rock");
-playerChoice = playerChoice.toLowerCase();
+// let playerChoice = prompt("What is your choice? ", "rock");
+// playerChoice = playerChoice.toLowerCase();
 
 
 // Then the computer is gonna output one of the same three choices.
@@ -19,8 +23,8 @@ function getComputerChoice() {
 
 let computerChoice = getComputerChoice();
 
-console.log(playerChoice);
-console.log(computerChoice);
+// console.log(playerChoice);
+// console.log(computerChoice);
 
 // We're gonna play a round.
 
@@ -46,13 +50,44 @@ function playRound(playerChoice, computerChoice) {
 
 // console.log (playRound(playerChoice, computerChoice));
 
+//Next we're gonna make it more complex. We're gonna play 5 rounds.
+
+
+let win = 0;
+let lose = 0;
 function game() {
-    for (let i = 1; i <= 5; i++) {
-        if (i <= 5) {
-            playerChoice = prompt("What is your choice? ", "rock");
+    for (let i = 0; i < 5; i++) {
+        if (i < 5) {
+            let playerChoice = prompt("What is your choice? ", "rock");
+            playerChoice = playerChoice.toLowerCase();
             computerChoice = getComputerChoice();
-            console.log (playRound(playerChoice, computerChoice));
-        } 
+            let result = (playRound(playerChoice, computerChoice));
+            console.log(result);
+            // make a function that keeps track of how many times you win/lose
+            if (result.substr(0, 7) === "You win") {
+                win++;        
+            } else if (result.substr(0, 8) === "You lose") {
+                lose++;
+            }
+        } if (i === 4) {
+            finalResult();
+        }
     }
 }
 
+game();
+
+
+//At the end display the winner
+
+function finalResult() {
+    if (win > lose) {
+        return (`You're the winner, congratulations! You won ${win} times` );
+    } else if (win < lose) {
+        return `You lost ${lose} times, better luck next time :(`;
+    } else if (win === lose) {
+        return "It's a tie! So close. Better luck next time! :)";
+    }
+}
+
+console.log(finalResult());
